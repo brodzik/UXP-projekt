86e3e2e6b249ba99155377dbdf310796b5404fbd
psatala <psatala@mion.elka.pw.edu.pl>
2021-05-21 22:19:10 +0200

    Implement tuple to string parsing

--- src/common/LindaTuple.h
+++ src/common/LindaTuple.h
@@ -5,12 +5,50 @@
 #include "LindaParser.h"
 
 class LindaTuple {
+
+    std::string addEscaping(std::string text) { 
+        std::string toReturnString;
+        for(unsigned int i = 0; i < text.size(); ++i) {
+            if('\\' == text[i] || '\"' == text[i])
+                toReturnString += '\\';
+            toReturnString += text[i];
+        }
+        return toReturnString;
+    }
+
 public:
     std::vector<Value> values;
-    LindaTuple() = default;
+
     LindaTuple(std::string textToParse) {
         std::stringstream inStream(textToParse);
         LindaParser lindaParser(inStream);
         values = lindaParser.buildTuple();
+    }
+
+    LindaTuple(std::vector<Value> values): values(values) {}
+
+    std::string toString() {
+        std::string toReturnString = "(";
+        for(Value value: values) {
+            
+            if(const int* val = std::get_if<int>(&value)) {
+                toReturnString += std::to_string(*val) + ",";
+                continue;
+            }
+
+            if(const float* val = std::get_if<float>(&value)) {
+                toReturnString += std::to_string(*val) + ",";
+                continue;
+            }
+
+            if(const std::string* val = std::get_if<std::string>(&value)) {
+                toReturnString += "\"" + addEscaping(*val) + "\"" + ",";
+                continue;
+            }
+        }
+
+        toReturnString.pop_back();
+        toReturnString += ")";
+        return toReturnString;
     }
 };
--- src/tests/parsingTuples.cpp
+++ src/tests/parsingTuples.cpp
@@ -1,86 +1,130 @@
 #include "AuxiliaryFunctions.h"
 
 TEST(parsingTuples, basicInt) {
+    // forward
     LindaTuple lindaTuple(R"( (1) )");
     assertValue(lindaTuple.values[0], 1);
     GTEST_ASSERT_EQ(lindaTuple.values.size(), 1);
+
+    // reverse
+    GTEST_ASSERT_EQ(lindaTuple.toString(), R"((1))");
 }
 
 
 TEST(parsingTuples, basicFloat) {
+    // forward
     LindaTuple lindaTuple(R"( (3.14) )");
     assertValue(lindaTuple.values[0], 3.14f);
     GTEST_ASSERT_EQ(lindaTuple.values.size(), 1);
+
+    // reverse
+    GTEST_ASSERT_EQ(lindaTuple.toString(), R"((3.140000))");
 }
 
 
 TEST(parsingTuples, basicString) {
+    // forward
     LindaTuple lindaTuple(R"( ("abc") )");
     assertValue(lindaTuple.values[0], R"(abc)");
     GTEST_ASSERT_EQ(lindaTuple.values.size(), 1);
+
+    // reverse
+    GTEST_ASSERT_EQ(lindaTuple.toString(), R"(("abc"))");
 }
 
 
 TEST(parsingTuples, stringEscaping1) {
+    // forward
     LindaTuple lindaTuple(R"( ("\"") )");
     assertValue(lindaTuple.values[0], R"(")");
     GTEST_ASSERT_EQ(lindaTuple.values.size(), 1);
+
+    // reverse
+    GTEST_ASSERT_EQ(lindaTuple.toString(), R"(("\""))");
 }
 
 
 TEST(parsingTuples, stringEscaping2) {
+    // forward
     LindaTuple lindaTuple(R"( ("\\") )");
     assertValue(lindaTuple.values[0], R"(\)");
     GTEST_ASSERT_EQ(lindaTuple.values.size(), 1);
+
+    // reverse
+    GTEST_ASSERT_EQ(lindaTuple.toString(), R"(("\\"))");
 }
 
 TEST(parsingTuples, multipleInts) {
+    // forward
     LindaTuple lindaTuple(R"( (1, 2, 3) )");
     assertValue(lindaTuple.values[0], 1);
     assertValue(lindaTuple.values[1], 2);
     assertValue(lindaTuple.values[2], 3);
     GTEST_ASSERT_EQ(lindaTuple.values.size(), 3);
+
+    // reverse
+    GTEST_ASSERT_EQ(lindaTuple.toString(), R"((1,2,3))");
 }
 
 TEST(parsingTuples, multipleFloats) {
+    // forward
     LindaTuple lindaTuple(R"( (1.01, 2.02, 3.03) )");
     assertValue(lindaTuple.values[0], 1.01f);
     assertValue(lindaTuple.values[1], 2.02f);
     assertValue(lindaTuple.values[2], 3.03f);
     GTEST_ASSERT_EQ(lindaTuple.values.size(), 3);
+
+    // reverse
+    GTEST_ASSERT_EQ(lindaTuple.toString(), R"((1.010000,2.020000,3.030000))");
 }
 
 TEST(parsingTuples, multipleStrings) {
+    // forward
     LindaTuple lindaTuple(R"( ("a", "b", "c") )");
     assertValue(lindaTuple.values[0], R"(a)");
     assertValue(lindaTuple.values[1], R"(b)");
     assertValue(lindaTuple.values[2], R"(c)");
     GTEST_ASSERT_EQ(lindaTuple.values.size(), 3);
+
+    // reverse
+    GTEST_ASSERT_EQ(lindaTuple.toString(), R"(("a","b","c"))");
 }
 
 TEST(parsingTuples, weirdSpacing) {
+    // forward
     LindaTuple lindaTuple(R"( (1   ,        2,3) )");
     assertValue(lindaTuple.values[0], 1);
     assertValue(lindaTuple.values[1], 2);
     assertValue(lindaTuple.values[2], 3);
     GTEST_ASSERT_EQ(lindaTuple.values.size(), 3);
+
+    // reverse
+    GTEST_ASSERT_EQ(lindaTuple.toString(), R"((1,2,3))");
 }
 
 
 TEST(parsingTuples, differentTypes1) {
+    // forward
     LindaTuple lindaTuple(R"( (1, 2.02, "abc") )");
     assertValue(lindaTuple.values[0], 1);
     assertValue(lindaTuple.values[1], 2.02f);
     assertValue(lindaTuple.values[2], R"(abc)");
     GTEST_ASSERT_EQ(lindaTuple.values.size(), 3);
+
+    // reverse
+    GTEST_ASSERT_EQ(lindaTuple.toString(), R"((1,2.020000,"abc"))");
 }
 
 
 TEST(parsingTuples, differentTypes2) {
+    // forward
     LindaTuple lindaTuple(R"( ("text", 2, 3, 0.99) )");
     assertValue(lindaTuple.values[0], R"(text)");
     assertValue(lindaTuple.values[1], 2);
     assertValue(lindaTuple.values[2], 3);
     assertValue(lindaTuple.values[3], 0.99f);
     GTEST_ASSERT_EQ(lindaTuple.values.size(), 4);
+
+    // reverse
+    GTEST_ASSERT_EQ(lindaTuple.toString(), R"(("text",2,3,0.990000))");
 }
